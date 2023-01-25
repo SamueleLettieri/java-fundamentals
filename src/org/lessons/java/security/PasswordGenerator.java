78c5b2cc5ad6c7b883eeb9d90a4148e5c28e36a7
SamueleLettieri <samuele2073@gmail.com>
2023-01-25 15:27:34 +0100

    Genera password

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,13 @@
+package org.lessons.java.security;
+
+public class PasswordGenerator {
+	
+	public static void main(String[] args) {
+		String name;
+		
+		String lastName;
+		
+		String favoriteColor;
+	}
+	
+}
